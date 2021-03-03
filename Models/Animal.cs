6752ff38a3e9a5732b44dc6f830a087043d9dc5b
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-03-03 13:57:36 -0800

    modify animal model to work with database

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -3,13 +3,13 @@ using MySql.Data.MySqlClient;
 
 namespace AnimalShelter.Models
 {
-  public class Animal
-  {
-    public int AnimalId { get; set; }
-    public string Name { get; set; }
-    public string Gender { get; set; }
-    public DateTime DateOfAdmittance { get; set; }
-    public string Type { get; set; }
-    public string Breed { get; set; }
-  }
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfAdmittance { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+    }
 }
