e09b008e7322fa094e03653f7213660e17a6b471
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-03-08 13:19:57 -0800

    add readme and fix models to sort additions

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -5,38 +5,38 @@ using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
-  public class AnimalsController : Controller
-  {
-    private readonly AnimalShelterContext _db; //constructor
-    public AnimalsController(AnimalShelterContext db) 
+    public class AnimalsController : Controller
     {
-      _db = db;//sets new _db property
-    }
+        private readonly AnimalShelterContext _db; //constructor
+        public AnimalsController(AnimalShelterContext db)
+        {
+            _db = db;//sets new _db property
+        }
 
-    public ActionResult Index() //replaces GetAll()
-    {
-      List<Animal> model = _db.Animals.ToList();
-      return View(model);
-    }
+        public ActionResult Index() //replaces GetAll()
+        {
+            List<Animal> model = _db.Animals.OrderBy(animal => animal.Name).ToList();
+            return View(model);
+        }
 
-    public ActionResult Create() //GET request for creating a new task
-    {
-      return View();
-    }
+        public ActionResult Create() //GET request for creating a new task
+        {
+            return View();
+        }
 
-    [HttpPost]
-    public ActionResult Create(Animal animal)
-    {
-      _db.Animals.Add(animal);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        [HttpPost]
+        public ActionResult Create(Animal animal)
+        {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-    public ActionResult Details(int id)
-    {
-      Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
-      return View(thisAnimal);
+        public ActionResult Details(int id)
+        {
+            Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            return View(thisAnimal);
+        }
     }
-  }
 
 }
